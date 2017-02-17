ab3f231ad8b2c69badcad5db39394b0b897d6e7c
Dan Marshall <danmar@microsoft.com>
2017-02-16 20:05:29 -0800

    demo works with svg polygon and polyline

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+"use strict";
 var makerjs = require("makerjs");
-module.exports = function (svgPathData, distance, options) {
-    if (options === void 0) { options = {}; }
-    var model = makerjs.importer.fromSVGPathData(svgPathData);
-    var expanded = makerjs.model.expandPaths(model, distance, options.joints);
-    makerjs.model.originate(expanded);
-    makerjs.model.simplify(expanded);
-    return makerjs.exporter.toSVGPathData(expanded, false, options.origin || [0, 0]);
+module.exports = function (svgData, distance, options) {
+    var defaultOptions = { joints: 0, outside: true, tagName: 'path' };
+    var o = makerjs.extendObject(defaultOptions, options);
+    var closed = true;
+    var model;
+    switch (o.tagName) {
+        case 'polyline':
+            closed = false;
+        //fall through
+        case 'polygon':
+            model = makerjs.model.mirror(new makerjs.models.ConnectTheDots(closed, svgData), false, true);
+            break;
+        default:
+            model = makerjs.importer.fromSVGPathData(svgData);
+            break;
+    }
+    var scaleUp = 1000;
+    //scale up
+    makerjs.model.scale(model, scaleUp);
+    var outlined = makerjs.model.outline(model, distance * scaleUp, o.joints, !o.outside);
+    //var outlined = makerjs.model.expandPaths(model, distance * 1000);
+    makerjs.model.simplify(outlined);
+    //scale down
+    makerjs.model.scale(outlined, 1 / scaleUp);
+    return makerjs.exporter.toSVGPathData(outlined, false, [0, 0]);
 };
--- index.ts
+++ index.ts
@@ -1,12 +1,46 @@
-var makerjs = require("makerjs") as typeof MakerJs;
+import * as makerjs from "makerjs";
 
-module.exports = function (svgPathData: string, distance: number, options: { joints?: number, origin?: MakerJs.IPoint, outline?: number } = {}): string {
+export interface Options {
+    joints?: number;
+    outside?: boolean;
+    tagName?: 'path' | 'polygon' | 'polyline';
+}
 
-    var model = makerjs.importer.fromSVGPathData(svgPathData);
-    var expanded = makerjs.model.expandPaths(model, distance, options.joints);
+module.exports = function (svgData: string, distance: number, options?: Options): string {
 
-    makerjs.model.originate(expanded);
-    makerjs.model.simplify(expanded);
+    const defaultOptions: Options = { joints: 0, outside: true, tagName: 'path' };
 
-    return makerjs.exporter.toSVGPathData(expanded, false, options.origin || [0, 0]) as string;
+    var o = makerjs.extendObject(defaultOptions, options) as Options;
+
+    let closed = true;
+    let model: MakerJs.IModel;
+
+    switch (o.tagName) {
+        case 'polyline':
+            closed = false;
+            //fall through
+
+        case 'polygon':
+            model = makerjs.model.mirror(new makerjs.models.ConnectTheDots(closed, svgData), false, true);
+            break;
+
+        default:
+            model = makerjs.importer.fromSVGPathData(svgData);
+            break;
+    }
+
+    var scaleUp = 1000;
+
+    //scale up
+    makerjs.model.scale(model, scaleUp);
+
+    var outlined = makerjs.model.outline(model, distance * scaleUp, o.joints, !o.outside);
+    //var outlined = makerjs.model.expandPaths(model, distance * 1000);
+
+    makerjs.model.simplify(outlined);
+
+    //scale down
+    makerjs.model.scale(outlined, 1 / scaleUp);
+
+    return makerjs.exporter.toSVGPathData(outlined, false, [0, 0]) as string;
 }
